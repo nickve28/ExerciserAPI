abe099928d0df8243ea95d99741f63c939ae1fd5
nickve28 <nickvaneyk@gmail.com>
2016-02-04 21:55:07 +0100

    Code formatting/consistency in exercises.clj

--- src/exerciserapi/exercises.clj
+++ src/exerciserapi/exercises.clj
@@ -5,30 +5,29 @@
 
 (defn http-response 
   ([data status]
-   (http-response data status nil))
+    (http-response data status nil))
   ([data status message]
     {:body data :status status :message message})
-) 
 
 (let [conn (mg/connect)
-      db   (mg/get-db conn "exerciser")
-      coll "exercises"]
+  db (mg/get-db conn "exerciser")
+  coll "exercises"]
 
   (defn bson-to-str [entry k]
     "Provides a workaround for chesire not supporting BSON, making it a string instead"
     (assoc entry k (clojure.core/str (k entry)) ))
 
   (defn id-to-str [entry]
-   "Shorthand for id conversion"
-   (bson-to-str entry :_id))
+    "Shorthand for id conversion"
+    (bson-to-str entry :_id))
 
   (defn get-exercises [params]
    "Retrieves all exercises, allows filter"
     (let [result (mc/find-maps db coll params)]
       (http-response (map (fn [entry]
-         (id-to-str entry)) 
-         result
-      ) 200)))
+        (id-to-str entry))
+        result
+        200))))
 
   (defn get-exercise [id]
     "Finds exercise based on id"
