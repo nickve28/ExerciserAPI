b51397398255d02f64b311b68cc944c5e51526cb
nickve28 <nickvaneyk@gmail.com>
2016-02-06 23:18:26 +0100

    Token authorization working for POST + PUT exercises

--- src/exerciserapi/exercises.clj
+++ src/exerciserapi/exercises.clj
@@ -2,7 +2,9 @@
   (:require [monger.core :as mg]
             [monger.collection :as mc]
             [validateur.validation :refer :all]
-            [exerciserapi.bson :refer :all])
+            [exerciserapi.bson :refer :all]
+            [clj-time.core :as time]
+            [buddy.auth :refer [authenticated? throw-unauthorized]])
   (:import org.bson.types.ObjectId))
 
 (def validations
@@ -35,21 +37,27 @@
     (let [result (mc/find-one-as-map db coll {:_id (ObjectId. id)})]
       (http-response (id-to-str result) 200)))
   
-  (defn save-exercise [data]
+  (defn save-exercise [request]
     "Saves an exercise"
-    (if (valid-record? data)
-      (let [result (mc/insert-and-return db coll (select-keys data [:name :category]))]
-        (http-response (id-to-str result) 201))
-      (http-response {:result (validations data) } 400)))
+    (if-not (authenticated? request)
+      (http-response {:message "You need to be authorized"} 401)
+      (let [data (:body request)]
+        (if (valid-record? data)
+          (let [result (mc/insert-and-return db coll (select-keys data [:name :category]))]
+            (http-response (id-to-str result) 201))
+          (http-response {:result (validations data) } 400)))))
  
-  (defn update-exercise [id data]
+  (defn update-exercise [request]
     "Updates the exercise with the given id"
-    (if (valid-record? data)
-      (let [result (mc/update-by-id db coll (ObjectId. id) (select-keys data [:name :category]))]
-        (http-response {:result "Update successfull"}  200))
-      (http-response {:result (validations data) } 400)))
+    (if-not (authenticated? request)
+      (http-response {:message "You need to be authorized"} 401)
+      (let [id (:id (:params request))
+            data (:body request)]
+        (if (valid-record? data)
+          (let [result (mc/update-by-id db coll (ObjectId. id) (select-keys data [:name :category]))]
+            (http-response {:result "Update successfull"}  200)))
+        (http-response {:result (validations data) } 400))))
 
   (defn delete-exercise [id]
     "Deletes the exercise"
-    (let [result (mc/remove-by-id db coll (ObjectId. id))] (http-response {:result "Deleted"} 200)))
-)
+    (let [result (mc/remove-by-id db coll (ObjectId. id))] (http-response {:result "Deleted"} 200))))
--- src/exerciserapi/handler.clj
+++ src/exerciserapi/handler.clj
@@ -4,31 +4,32 @@
             [ring.middleware.json :as middleware]
             [compojure.route :as route]
             [exerciserapi.exercises :as exercises]
-            [exerciserapi.authentication :as auth]))
+            [exerciserapi.authentication :as auth]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]))
 
 (require  '[buddy.auth.backends.token :refer (jws-backend)])
-(require  '[buddy.auth.middleware :refer (wrap-authentication)])
 
 (def prefix "/api")
 (def secret (:secret (with-open [r (clojure.java.io/reader "./config/secret.clj")]
                (read (java.io.PushbackReader. r))))) 
 
-(def backend (jws-backend {:secret secret}))
+(def backend (jws-backend {:secret secret :options {:alg :hs512}}))
 
 (defroutes app-routes
   (context (str prefix "/auth") [] (defroutes authentication-routes
     (POST "/login" request (auth/login-handler request))))
   (context (str prefix "/exercises") [] (defroutes exercises-routes
     (GET "/" {params :params} (exercises/get-exercises params))
-    (POST "/" request (exercises/save-exercise (:body request))) 
+    (POST "/" [] exercises/save-exercise) 
     (context "/:id" [id] (defroutes exercise-routes
       (GET "/" [] (exercises/get-exercise id))
-      (PUT "/" {params :params body :body} (exercises/update-exercise (:id params) body))
+      (PUT "/" [] exercises/update-exercise)
       (DELETE "/" [] (exercises/delete-exercise id))))))
    (route/not-found "Not Found"))
 
 (def app
   (-> (handler/api app-routes)
+      (wrap-authorization backend)
       (wrap-authentication backend)
       (middleware/wrap-json-body {:keywords? true})
       middleware/wrap-json-response))
