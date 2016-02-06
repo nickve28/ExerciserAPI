dc866fc1f276cf759b9d1b4dfe3d7147bea99ee5
nickve28 <nickvaneyk@gmail.com>
2016-02-06 21:58:52 +0100

    User being read from database. To do: password hashing

--- src/exerciserapi/authentication.clj
+++ src/exerciserapi/authentication.clj
@@ -1,15 +1,26 @@
-(ns exerciserapi.authentication)
+(ns exerciserapi.authentication
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [validateur.validation :refer :all]
+            [exerciserapi.bson :refer :all])
+  (:import org.bson.types.ObjectId))
 
 (require '[buddy.sign.jws :as jws])
 (require '[cheshire.core :as json])
 
 (def secret (:secret (with-open [r (clojure.java.io/reader "./config/secret.clj")]
-               (read (java.io.PushbackReader. r))))) 
+               (read (java.io.PushbackReader. r)))))
 
-(defn login-handler
-  [request]
-  (let [data (:params request)
-        user {:username "foo" :password "bar" :id 1}
-        token (jws/sign {:user (:id user)} secret)]
-    {:status 200
-     :body (json/encode {:token token})}))
+(let [conn (mg/connect)
+  db (mg/get-db conn "exerciser")
+  coll "user"]
+
+  (defn login-handler
+    [request]
+    (let [data (:body request)
+          user  (mc/find-one-as-map db coll {:username (:username data) :password (:password data) })
+          id    (clojure.core/str (:_id user))
+          token (jws/sign {:user id} secret)]
+      (if (nil? user)
+          {:status 400 :body "Incorrect credentials"}
+          {:status 200 :body (json/encode {:token token}) }))))
