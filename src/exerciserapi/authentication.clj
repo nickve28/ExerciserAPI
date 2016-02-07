beb171a506689ca2cc65b5236bf3909b21860487
nickve28 <nickvaneyk@gmail.com>
2016-02-07 12:52:46 +0100

    Validation for username+password not empty

--- src/exerciserapi/authentication.clj
+++ src/exerciserapi/authentication.clj
@@ -9,21 +9,28 @@
 (require '[buddy.sign.jws :as jws])
 (require '[cheshire.core :as json])
 
+(def validations (validation-set
+                   (presence-of :username)
+                   (presence-of :password)))
+
+(def valid-record? (partial valid? validations))
+
 (def secret (:secret (with-open [r (clojure.java.io/reader "./config/secret.clj")]
                (read (java.io.PushbackReader. r)))))
 
 (let [conn (mg/connect)
   db (mg/get-db conn "exerciser")
   coll "user"]
-
   (defn login-handler
     [request]
-    (let [data (:body request)
-          user  (mc/find-one-as-map db coll {:username (:username data) :password (:password data) })]
-      (if (nil? user)
+    (if (valid-record? (:body request))
+      (let [data (:body request)
+            user  (mc/find-one-as-map db coll {:username (:username data) :password (:password data) })]
+        (if (nil? user)
           {:status 400 :body "Incorrect credentials"}
           (let [id (clojure.core/str (:_id user))
                 claims {:user id
                         :exp (time/plus (time/now) (time/seconds 3600))}
                 token (jws/sign claims secret {:alg :hs512})]
-            {:status 200 :body (json/encode {:token token}) })))))
+            {:status 200 :body (json/encode {:token token}) })))
+      {:status 400 :body (validations (:body request))})))
